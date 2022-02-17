97d5dbef92e8fa5c479821e76cac0bfc4cb0da0c
JavierHM-Audioslave <javorengero2000@yahoo.com.ar>
2022-02-17 15:31:20 -0300

    Modifying the sentence in Button component

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
                 
                 <Row className="mt-3">
                     <Col>
-                        <Button variant="info" onClick={ () => setFilteredUsers(orderByName(filteredUsers || users)) }>Order by ascending full name</Button>
+                        <Button variant="info" onClick={ () => setFilteredUsers(orderByName(filteredUsers || users)) }>Order by ascending last name</Button>
                     </Col>
                 </Row>
             </Form>
